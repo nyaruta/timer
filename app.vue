6d7c09836e76baeaa0793664c2abf7906b542f00
purofle <purofle@gmail.com>
2024-06-12 22:42:19 +0800

    init2

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <Timer />
+  </div>
+</template>
--- components/Timer.vue
+++ components/Timer.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="timer-container">
     <h2 class="text-2xl leading-7 font-semibold">
-        和 <a href="https://t.me/purolle" target="_blank"  style="color: black" class="button--doc text-green-500 hover:underline">@Purofle</a> 已经相恋
-      </h2>
+      和
+      <a
+        href="https://t.me/purolle"
+        target="_blank"
+        style="color: black"
+        class="button--doc text-green-500 hover:underline"
+        >@Purofle</a
+      >
+      已经相恋
+    </h2>
     <div class="timer">{{ formattedTime }}</div>
   </div>
 </template>
@@ -11,10 +19,10 @@
 export default {
   data() {
     return {
-      startTime: new Date('2024-05-21T00:00:00Z'), // 固定的开始时间
+      startTime: new Date("2024-05-21T00:00:00Z"), // 固定的开始时间
       currentTime: new Date(),
-      timer: null
-    }
+      timer: null,
+    };
   },
   computed: {
     formattedTime() {
@@ -24,25 +32,29 @@ export default {
       const minutes = Math.floor((diff % 3600) / 60);
       const seconds = diff % 60;
       return (
-        (days < 10 ? '0' + days : days) + '天 ' +
-        (hours < 10 ? '0' + hours : hours) + '小时 ' +
-        (minutes < 10 ? '0' + minutes : minutes) + '分钟 ' +
-        (seconds < 10 ? '0' + seconds : seconds) + '秒'
+        (days < 10 ? "0" + days : days) +
+        "天 " +
+        (hours < 10 ? "0" + hours : hours) +
+        "小时 " +
+        (minutes < 10 ? "0" + minutes : minutes) +
+        "分钟 " +
+        (seconds < 10 ? "0" + seconds : seconds) +
+        "秒"
       );
-    }
+    },
   },
   methods: {
     updateTime() {
       this.currentTime = new Date();
-    }
+    },
   },
   mounted() {
     this.timer = setInterval(this.updateTime, 1000);
   },
   beforeDestroy() {
     clearInterval(this.timer);
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
@@ -53,9 +65,9 @@ export default {
   justify-content: center;
   height: 100vh;
 }
+
 .timer {
   font-size: 48px;
   margin-bottom: 20px;
 }
-
 </style>
